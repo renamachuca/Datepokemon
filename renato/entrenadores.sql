b9709392dbdbfbab15e27d77c4e804410cfe583b
Renato Machuca <machucarenato8@gmail.com>
2024-07-29 08:02:32 -0400

    Add files via upload

--- renato/entrenadores.sql
+++ renato/entrenadores.sql
@@ -1,11 +1,21 @@
-SELECT * FROM "Entrenadores"
-ORDER BY "id_entrenador" ASC	
-INSERT INTO Entrenadores VALUES (15,' Israel',13, 'Masculino');
-INSERT INTO Entrenadores VALUES (16,'Bruno',13, 'Masculino');
-DELETE FROM Entrenadores
-WHERE id_entrenador = 15
-UPDATE Entrenadores
-SET "Nombre" = 'blaine'
-WHERE id_pokemon = 4;
+SELECT * FROM public.Entrenadores
+ORDER BY id_entrenador ASC 
+--Insertar
+INSERT INTO public."Entrenadores (Nombre, Edad, Sexo)
+VALUES ('Ash Ketchum', 10, 'Masculino');
 
+--Leer 
+SELECT * FROM public."Entrenadores";
+
+-- Leer un registro específico
+SELECT * FROM public."Entrenadores
+WHERE Nombre = 'Ash Ketchum';
+
+--Actualizar 
+UPDATE public."Entrenadores"
+SET Edad = 11
+WHERE Nombre = 'Ash Ketchum';
+--Eliminar 
+DELETE FROM public."Entrenadores"
+WHERE Nombre = 'Ash Ketchum';
 
